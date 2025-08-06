742e3e2035567cf392ff11c1a3c48788cecd212b
MandulaSiddardha <145456488+MandulaSiddardha@users.noreply.github.com>
2025-08-06 19:19:07 +0530

    Create reversestring.java

---
+++ reversestring.java
@@ -0,0 +1,11 @@
+class ReverseString {
+    public static void main(String[] args) {
+        String original = "Hello World";
+        String reversed = "";
+        for (int i=0;i<original.length();i++) {
+            reversed =original.charAt(i)+reversed;
+        }
+        System.out.println("Original: " + original);
+        System.out.println("Reversed: " + reversed);
+    }
+}
